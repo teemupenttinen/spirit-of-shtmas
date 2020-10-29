d618acb17be11704cc3686e44d15733de602f8e6
Teemu Penttinen <teemusp@outlook.com>
2020-10-29 22:14:21 +0200

    WIP: basic responsive menu

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,11 @@ function App() {
       <header className="App-header">
         <h1>Spirit of Shitmas</h1>
       </header>
+      <div className="Menu">
+        <button>Start</button>
+        <button>Guide</button>
+        <button>Settings</button>
+      </div>
       <Snowflakes/>
     </div>
   );
